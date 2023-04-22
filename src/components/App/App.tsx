01d7af76cfdaa21b49302ebb051031c597ec7553
Ildar <ildar9@list.ru>
2023-04-23 00:38:06 +0300

    add base styles

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Layout } from 'components';
 
-export const App = () => {
+export const App: React.FC = () => {
   return (
-    <div>App</div>
+    <div className='App'>
+      <Layout>
+        <h1>React/ts-ui-library</h1>
+      </Layout>
+    </div>
   )
 }
---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,15 @@
+import React, { ReactNode } from 'react'
+
+interface ILayoutProps {
+  children?: ReactNode;
+}
+
+export const Layout: React.FC<ILayoutProps> = ({ children }) => {
+  return (
+    <div className='Layout'>
+      <div className='Layout-Container'>
+        {children}
+      </div>
+    </div>
+  )
+}
